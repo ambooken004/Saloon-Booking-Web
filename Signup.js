24ace5d9b21ae3ab2a2f0d1283027b30f98551be
ambooken004 <joeambooken1@gmail.com>
2023-03-31 19:43:32 +0530

    Code for unchecked box is successful

--- Signup.js
+++ Signup.js
@@ -2,32 +2,32 @@ const homeBtn = document.getElementById("home-btn");
 const checkBox = document.getElementById("checkbox");
 const registerBtn = document.getElementById("register-btn");
 registerBtn.disabled=true;
-registerBtn.style.backgroundColor="grey";
 
 const changeColor = (event) => {
-        event.target.style.backgroundColor= "purple";
+        event.target.style.backgroundColor= "blue";
     };
 const originalColor = (event) => {
     event.target.style= "none";
 };
 
-const disableRegisBtn =(event) => {
-    const checkBoxChecked = event;
-    if(checkBoxChecked)
-    {
-        registerBtn.disabled=false;
-        registerBtn.style.backgroundColor="black";
-        checkBox.disabled=true;
-    }
-
-    // Write code for the condition (if checkbox is unchecked after it's  been checked)...  LATER
-}
-const oncli = () => {
+const onClicked = () => {
     alert("Thanks for registering...");
 }
 
-
 homeBtn.addEventListener("mouseover",changeColor);
 homeBtn.addEventListener("mouseleave",originalColor);
-checkBox.addEventListener("click",disableRegisBtn);
-registerBtn.addEventListener("click",oncli);
+checkBox.addEventListener("change", function()
+{
+    if(this.checked)
+    {
+        registerBtn.disabled=false;
+        registerBtn.style.backgroundColor="green";
+
+    }
+    else
+    {
+        registerBtn.disabled=true;
+        registerBtn.style.backgroundColor="black";
+    }
+});
+registerBtn.addEventListener("click",onClicked);
